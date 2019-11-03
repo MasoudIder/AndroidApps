db2cefef18168c27a1e88459c3e99e088014db38
administrator <masoudider@yahoo.com>
2019-11-03 19:43:46 +0330

    taxiApp initializing

--- app/src/main/java/com/example/taxiapp/MainActivity.java
+++ app/src/main/java/com/example/taxiapp/MainActivity.java
@@ -1,14 +1,78 @@
 package com.example.taxiapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
+    public static final String TAG = "TAG";
+    private static final int REQEST_CODE = 1000;
+
+    private GoogleApiClient googleApiClient;
+    private Location location;
+
+    private TextView txtLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtLocation=findViewById(R.id.txtLocation);
+
+        googleApiClient= new GoogleApiClient.Builder(MainActivity.this)
+                .addConnectionCallbacks(MainActivity.this)
+                .addOnConnectionFailedListener(MainActivity.this)
+                .addApi(LocationServices.API).build();
+
+
     }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+    }
+
+    //custom methods
+
+    private void showLocatioan(){
+
+        int permissionCheck= ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION);
+
+        if(permissionCheck== PackageManager.PERMISSION_GRANTED){
+
+
+
+
+
+        }else {
+            txtLocation.setText("The app is not allowed to access location");
+            ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},1);
+        }
+
+    }
+
+
 }
+
